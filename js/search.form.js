cac02fb674c2b5a6e9b8a0df6d2aa25d8d9ba290
RedRedfox <>
2017-12-13 19:36:15 +0500

    Правки скриптов формы при ошибке.

--- js/search.form.js
+++ js/search.form.js
@@ -1,6 +1,5 @@
 var link = document.querySelector(".search-button");
 var formSearch = document.querySelector(".search-form");
-var form = document.querySelector("form");
 var arrivalDate = formSearch.querySelector("[name=arrival-date]");
 var departureDate = formSearch.querySelector("[name=departure-date]");
 var jsadults = formSearch.querySelector("[name=adults]");
@@ -27,9 +26,11 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-form.addEventListener("submit", function (evt) {
+formSearch.addEventListener("submit", function (evt) {
   if (!arrivalDate.value || !departureDate.value || !jsadults.value || !jskids.value) {
   evt.preventDefault();
+  formSearch.classList.remove("form-error");
+  formSearch.offsetWidth=formSearch.offsetWidth;
   formSearch.classList.add("form-error");
   }
 });
